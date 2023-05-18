086b277d911677af708dfc5fe780fe09d49f8b9c
Mugil <mugil.choi@gmail.com>
2023-05-18 17:05:01 -0400

    finished 01

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null)
+    this.count = 0
   }
 
   hash(key) {
@@ -30,22 +31,73 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    let keyPair = new KeyValuePair(key, value);
+    let Index = this.hashMod(key);
+    if(this.data[Index] !== null){
+      let current = this.data[Index];
+      while(current){
+        if(current.key == key){
+          current.value = value;
+          return;
+        }
+        else{
+          current = current.next
+        }
+      }
+      keyPair.next = this.data[Index];
+      this.data[Index] = keyPair;
+      this.count++;
+    }
+    else{
+      this.data[Index] = keyPair;
+      this.count++;
+    }
   }
 
 
   read(key) {
-    // Your code here
+    let Index = this.hashMod(key);
+    let current = this.data[Index];
+    while (current) {
+      if (current.key == key){
+        return current.value;
+      }
+      current = current.next
+    }
+    return undefined
   }
 
 
   resize() {
-    // Your code here
+    const oldData = this.data;
+    this.capacity = this.capacity * 2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+    for (let ele of oldData) {
+      while(ele) {
+        this.insert(ele.key, ele.value);
+        ele = ele.next;
+      }
+    }
+
   }
 
 
   delete(key) {
-    // Your code here
+    let Index = this.hashMod(key);
+    let current = this.data[Index];
+    while (current){
+      if(current.key == key){
+        current.key = undefined;
+        current.value = undefined;
+        this.count--;
+        if (!current){
+          return;
+        }
+      }
+      current = current.next;
+    }
+    return "Key not found";
   }
 }
 
